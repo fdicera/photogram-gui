00e578035148c709e382de7e218bbbb0abe03b17
fdicera <67300276+fdicera@users.noreply.github.com>
2023-11-26 21:35:25 +0000

    Created users page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,14 @@ class UsersController < ApplicationController
     @list_of_users = mathing_users.order({ :username => :asc})
     render({ :template => "user_templates/index"})
   end
+
+  def show
+    #Parameters: {"path_username"=>"anisa"}
+    url_username = params.fetch("path_username")
+    matching_usernames = User.where({ :username => url_username })
+    @the_user = matching_usernames.at(0)
+    #if the_user == nil redirect_to("/404") else render... end
+
+    render({ :template => "user_templates/show"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 
   # name controller after the table plural, list of records index
   get("/users", { :controller => "users", :action => "index"})
+
+  get("/users/:path_username", { :controller => "users", :action => "show"})
 end
