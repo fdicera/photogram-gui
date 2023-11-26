5bb869ec919de09f8ac684f627e73e9ff420744e
fdicera <67300276+fdicera@users.noreply.github.com>
2023-11-26 22:14:49 +0000

    Added delete photo ability.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -17,4 +17,15 @@ class PhotosController < ApplicationController
     render({ :template => "photo_templates/show"})
 
   end
+
+  def bye
+    # Parameters: {"toast_id" => "785"}
+    the_id = params.fetch("toast_id")
+
+    matching_photos = Photo.where({:id => the_id})
+    the_photo = matching_photos.at(0)
+    the_photo.destroy
+    #render({ :template => "photo_templates/bye"})
+    redirect_to("/photos")
+  end
 end
